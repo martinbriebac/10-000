5b82fe5950451a00905bb99200313ae3e117645f
martinbriebac <105032631+martinbriebac@users.noreply.github.com>
2024-09-01 23:52:16 -0400

    Added setup and credits scenes

--- scripts/credits_scene.gd
+++ scripts/credits_scene.gd
@@ -5,3 +5,11 @@ func _ready():
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 1, 1).set_ease(Tween.EASE_IN_OUT)
 	tween.play()
+	
+	$Back.connect("pressed", Callable(self, "_on_back_pressed"))
+
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/setup_scene.tscn")
+
--- scripts/setup_scene.gd
+++ scripts/setup_scene.gd
@@ -5,7 +5,7 @@ var num_players = 1
 # Music volume slider
 var music_volume = 0.5
 
-func _ready():
+func _ready() -> void:
 	var viewport = get_viewport()
 	viewport.set_size(Vector2(2300, 1295))
 	
@@ -19,6 +19,12 @@ func _ready():
 	# Initialize the music volume slider
 	$MusicVolumeHSlider.value = music_volume
 
+	# Disconnect the button to the function
+	$ShowCredits.disconnect("pressed", Callable(self, "_on_CreditsButton_pressed"))
+	# Connect the button to the function
+	$ShowCredits.connect("pressed", Callable(self, "_on_CreditsButton_pressed"))
+
+
 func _on_NumPlayersOptionButton_item_selected(index):
 	num_players = index + 1
 	print("Number of players selected: ", num_players)
@@ -29,15 +35,13 @@ func _on_StartGameButton_pressed():
 	# Add code to start the game here
 	get_tree().change_scene("res://table.tscn")
 
-func _on_CreditsButton_pressed():
-	# Show the credits screen
-	print("Showing credits screen")
-	var credits_scene = load("res://scenes/credits_scene.tscn")
-	get_tree().get_root().add_child(credits_scene.instance())
-	self.hide()
+func _on_CreditsButton_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits_scene.tscn")
+	
 
 func _on_MusicVolumeHSlider_value_changed(value):
 	music_volume = value
 	print("Music volume set to: ", music_volume)
 	# Add code to update the music volume here
+
 
